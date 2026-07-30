a20d93d3f4cb85145858f8ddfb4d1bcdb7da14d8
yacheng159654-sys <yacheng159654@gmail.com>
2026-07-30 13:11:47 +0800

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,1 +1,27 @@
 
+fetch("products.json")
+.then(res => res.json())
+.then(products => {
+
+const container =
+document.getElementById("product-list");
+
+products.forEach(product => {
+
+container.innerHTML += `
+
+<div class="card">
+
+<img src="${product.image}">
+
+<h3>${product.name}</h3>
+
+<p>NT$${product.price}</p>
+
+</div>
+
+`;
+
+});
+
+});
